8eeeb676d2b5f1a99c0562276a5a36fd93531e2c
Ashish <ashish.mi.mishra@oracle.com>
2022-05-10 01:06:05 +0530

    homepage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
